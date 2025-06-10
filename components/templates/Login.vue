f8d19254273dc814875ac605027199d7042c4645
yudayuda78 <85638796+yudayuda78@users.noreply.github.com>
2025-06-10 16:15:59 +0700

    Style : add login form

---
+++ components/templates/Login.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+  <div class="w-[400px] mx-auto space-y-4 p-6 bg-white rounded-xl shadow-md">
+
+    <h2>Masuk ke MentorKita</h2>
+    <p>Silahkan isi form berikut untuk melanjutkan</p>
+
+ 
+
+    <Input name="email" type="email" placeholder="Masukkan email">
+      Email
+    </Input>
+
+    <Input name="password" type="password" placeholder="Masukkan password">
+      Password
+    </Input>
+
+  
+
+    <Button class="w-full">Login</Button>
+    <Button class="w-full">Daftar Menggunakan Google</Button>
+    
+  </div>
+</template>
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -2,4 +2,5 @@
 </script>
 
 <template>
+    <Login/>
 </template>
