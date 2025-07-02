8688e340b44fe51b8ece7a73a01ee1287d9384f6
yudayuda78 <85638796+yudayuda78@users.noreply.github.com>
2025-07-02 23:21:59 +0700

    Feat : add update tryout payment

---
+++ server/api/tryout/payment/[id].put.js
@@ -0,0 +1,32 @@
+export default defineEventHandler(async (event) => {
+  const id = parseInt(event.context.params.id) // Ambil ID dari URL
+  const body = await readBody(event)            // Ambil body dari request
+
+  if (isNaN(id) || typeof body.isPaid !== 'boolean') {
+    return {
+      error: true,
+      message: 'ID tidak valid atau data isPaid tidak sesuai'
+    }
+  }
+
+  try {
+    const updated = await prisma.paymentSnbtTryout.update({
+      where: { id },
+      data: {
+        isPaid: body.isPaid
+      }
+    })
+
+    return {
+      success: true,
+      message: 'Status berhasil diperbarui',
+      data: updated
+    }
+  } catch (error) {
+    return {
+      error: true,
+      message: 'Gagal menyimpan status',
+      detail: error.message
+    }
+  }
+})
