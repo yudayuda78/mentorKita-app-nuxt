d7578bacfd7512d9ef3fc089a33ef3a3861cb0c8
yudayuda78 <85638796+yudayuda78@users.noreply.github.com>
2025-07-09 15:08:25 +0700

    Feat : add image for sbntsoal model

---
+++ prisma/migrations/20250709080743_edit_snbt_model/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "SnbtSoal" ADD COLUMN     "optionAImage" TEXT,
+ADD COLUMN     "optionBImage" TEXT,
+ADD COLUMN     "optionCImage" TEXT,
+ADD COLUMN     "optionDImage" TEXT,
+ADD COLUMN     "optionEImage" TEXT,
+ADD COLUMN     "questionImage" TEXT;
