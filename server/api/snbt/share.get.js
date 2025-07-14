22704efa42901ed80d4ac65d12117f6d373ca56c
yudayuda78 <85638796+yudayuda78@users.noreply.github.com>
2025-07-14 15:23:53 +0700

    Fix : SNBTShare

--- server/api/snbt/share.get.js
+++ server/api/snbt/share.get.js
@@ -1,6 +1,15 @@
 export default defineEventHandler(async (event) => {
       const query = getQuery(event)
-  const user = await getUserFromToken(event) // ← ambil user dari cookie/JWT
+  const user = await getUserFromToken(event)
+  
+   if (!user) {
+    throw createError({
+      statusCode: 401,
+      statusMessage: 'Unauthorized: User not found or token invalid'
+    })
+  }
+
+
   const userId = user.id
   const snbtTryoutId = parseInt(query.snbtTryoutId)
     const getShared = await prisma.shareSnbt.findFirst({
