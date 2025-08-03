02b62abcf31fd5430896fb9b0aa7a08a1b5ceb9c
yudayuda78 <85638796+yudayuda78@users.noreply.github.com>
2025-08-03 16:03:10 +0700

    Feat : download soal API

---
+++ server/api/downloadsoal/index.js
@@ -0,0 +1,13 @@
+export default defineEventHandler(async (event) => {
+    const soal = await prisma.DownloadSoal.findMany({
+        orderBy: {
+            createdAt: 'desc'
+        },
+    })
+
+    return {
+        status: 200,
+        message: 'Berhasil mendapatkan data download soal',
+        data: soal
+    }
+})
