b1c7f4a627a40b994cfebf4bdac127777ab951c4
yudayuda78 <85638796+yudayuda78@users.noreply.github.com>
2025-08-16 22:52:42 +0700

    Feat : root icon

--- app.vue
+++ app.vue
@@ -1,11 +1,16 @@
 <script setup>
 useHead({
+ title: 'MentorKita',
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/mentorkita.ico' },
+    { rel: 'apple-touch-icon', href: '/mentorkita.png' }
+  ],
   meta: [
-    // Google Search Console verification
+ 
     { name: 'google-site-verification', content: "ca-pub-4270039076501112" }
   ],
   script: [
-    // Google AdSense script
+ 
     { 
       src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4270039076501112',
       async: true,
