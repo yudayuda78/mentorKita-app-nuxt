0c1ace4b16848caf6faa6919a2473d745d2bf1f1
yudayuda78 <85638796+yudayuda78@users.noreply.github.com>
2025-08-16 08:57:33 +0700

    Feat : google ads

---
+++ app.vue
@@ -0,0 +1,20 @@
+<script setup>
+useHead({
+  meta: [
+    // Google Search Console verification
+    { name: 'google-site-verification', content: "ca-pub-4270039076501112" }
+  ],
+  script: [
+    // Google AdSense script
+    { 
+      src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4270039076501112',
+      async: true,
+      crossorigin: 'anonymous'
+    }
+  ]
+})
+</script>
+
+<template>
+  <NuxtPage />
+</template>
